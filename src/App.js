47d49e8e9392641895e08e441c50a5a1813323a4
r584627913 <r584627913@gmail.com>
2021-03-28 22:42:34 +0800

    Created React.js Project.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Created React.js Project.
         </p>
         <a
           className="App-link"
