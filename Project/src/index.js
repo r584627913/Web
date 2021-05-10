515949218846360566037cabd15d31066cb0ec06
r584627913 <r584627913@gmail.com>
2021-05-10 23:15:09 +0800

    update

--- Project/src/index.js
+++ Project/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import LoginControl from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <LoginControl />,
+  <App />,
   document.getElementById('root')
 );
 
