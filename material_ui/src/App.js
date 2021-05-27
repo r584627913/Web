610c412682de4dc82454c2a4b2030ee824000399
r584627913 <r584627913@gmail.com>
2021-05-27 10:38:53 +0800

    checkBox

--- material_ui/src/App.js
+++ material_ui/src/App.js
@@ -2,18 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Button variant="outlined" color="primary">
-          Primary
-        </Button>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+
+    const [checked, setChecked] = React.useState(true);
+
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Button variant="outlined" color="primary">
+                    Primary
+                </Button>
+                <Checkbox
+                    checked={checked}
+                    onChange={handleChange}
+                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
 }
 
 export default App;
